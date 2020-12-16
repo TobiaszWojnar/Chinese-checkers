7b9e64b26dec360aecc4cd4003d5ecb059e1a3b6
IgnacyKowalewski <ignacykowalewski314@gmail.com>
2020-12-16 14:37:14 +0100

    Fixed containing pawn in opposite corner after it enters it

--- src/main/java/board/corners/Corner.java
+++ src/main/java/board/corners/Corner.java
@@ -2,9 +2,7 @@ package board.corners;
 
 import board.IntPoint;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.function.Consumer;
 
 public abstract class Corner {
     public List<IntPoint> points;
