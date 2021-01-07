4b75c19e7692caf7c07f6612c7cc705faa24a18e
IgnacyKowalewski <ignacykowalewski314@gmail.com>
2021-01-07 12:43:21 +0100

    Added instruction for launching with maven

--- src/test/java/server/OppositeCornerTest.java
+++ src/test/java/server/OppositeCornerTest.java
@@ -27,10 +27,6 @@ public class OppositeCornerTest {
 
         LogicUnitAllFilled logic = new LogicUnitAllFilled(testBoard, corners);
 
-        //for (corners.get(Field.Player2).)
-
-        /*testBoard.show();
-        checkBoard.show();*/
 
         testBoard.setField(13, 13, Field.Empty);
         testBoard.setField(12, 16, Field.Empty);
@@ -41,22 +37,12 @@ public class OppositeCornerTest {
         checkBoard.setField(12, 16, Field.Possible);
         checkBoard.setField(14, 14, Field.Chosen);
 
-        //checkBoard.show();
-
         logic.highlightPossible(14, 14, Field.Player1);
 
 
-        for (IntPoint point : UpperCorner.getInstance().points) {
-            System.out.println("x " + point.getX() + " " + point.getY());
-        }
-
-        boolean temp = corners.get(Field.Player2).has(12, 2);
-        System.out.println(temp);
-        System.out.println(corners.size());
-
         for (int x = 0; x < 25; x++)
             for (int y = 0; y < 17; y++) {
-                System.out.println(x + " " + y);
+                //System.out.println(x + " " + y);
                 assertEquals(checkBoard.getField(x, y), testBoard.getField(x, y));
             }
     }
